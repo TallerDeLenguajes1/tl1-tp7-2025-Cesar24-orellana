e399f546e25479f947e85b90094250ff12f28625
Cesar Orellana <cesarlorellana184@gmail.com>
2025-06-01 14:18:35 -0300

    Cargar-datos

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Personal;
+using static Personal.Empleado;
 
-var empleado = new Empleado("Jorge", "Acosta", new DateTime(24,04,1985), 'S', new DateTime(23,02,2020), 5435884, cargo.Auxiliar);
+Empleado empleado = new Empleado("Jorge", "Acosta", new DateTime(1985,4,19), 'S', new DateTime(2020,2,20), 5435884, Cargos.Auxiliar);
 
 //empleados.Add(new Empleado("Jorge", "Acosta", DateTime(24,04,1985), "S", DateTime(23,02,2020), 5435884, Cargos.Especialista))
 //empleado.Empleado("Jorge", "Acosta", 24.04.1985, "S", 23.02.2020, 5435884, 1);
